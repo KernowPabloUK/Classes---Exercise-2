a977b8b4ebe79e106c9aac2dddae3d6339c6a212
KernowPabloUK <PaulMorganUK1@outlook.com>
2022-10-08 16:07:17 +0100

    Add project files.

---
+++ Classes - Exercise 2/Post.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes___Exercise_2
+{
+    internal class Post
+    {
+        private string Title;
+        private string Description;
+        private DateTime CreationDateTime;
+        private int VoteValue;
+        private int UpVotes; //DOESN'T NEED TO BE A FIELD
+        private int DownVotes; //DOESN'T NEED TO BE A FIELD
+
+        internal Post()
+        {
+            this.Title = string.Empty;
+            this.Description = string.Empty;
+            this.CreationDateTime = DateTime.Now;
+            this.VoteValue = 0;
+
+        }
+
+        internal void CommandList()
+        {
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine("The commands for this program are:");
+            Console.WriteLine("NAME / UPVOTE / DOWNVOTE / COUNT / PREVIEW");
+            Console.WriteLine();
+            Console.WriteLine("NAME = Declare Title & Description of Post (can be used for editing also)");
+            Console.WriteLine("UPVOTE = Declare value of Up-Votes, this can be incremented multiple times");
+            Console.WriteLine("DOWNVOTE = Declare value of Down-Votes, this can be incremented multiple times");
+            Console.WriteLine("COUNT = Display the current net value of votes");
+            Console.WriteLine("PREVIEW = Preview the post");
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine("To show these commands again, please type COMMANDS");
+            Console.WriteLine("------------------------------------------------------------------------------");
+        }
+
+        internal void NameAndDescribePost()
+        {
+            Console.WriteLine($"Please enter a \"TITLE\" for your Post.");
+            this.Title = Console.ReadLine();
+            Console.WriteLine($"Please enter a \"DESCRIPTION\" of your Post.");
+            this.Description = Console.ReadLine();
+        }
+
+        internal void UpVotePost()
+        {
+            Console.WriteLine($"How many Up-Votes would you like to give this post?");
+            this.UpVotes = Convert.ToInt32(Console.ReadLine());
+            this.VoteValue += this.UpVotes;
+        }
+
+        internal void DownVotePost()
+        {
+            Console.WriteLine($"How many Down-Votes would you like to give this post?");
+            this.DownVotes = Convert.ToInt32(Console.ReadLine());
+            this.VoteValue -= this.DownVotes;
+        }
+
+        internal void DisplayCurrentVoteValue()
+        {
+            Console.WriteLine($"The current Vote value of this post is: {VoteValue}");
+        }
+
+        internal void PreviewPost()
+        {
+            Console.WriteLine($"Post Title: {this.Title}");
+            Console.WriteLine($"Post Description: {this.Description}");
+            Console.WriteLine($"Created on: {this.CreationDateTime}");
+            Console.WriteLine($"Down-Votes: {this.VoteValue}"); //CHANGE NAME
+
+        }
+    }
+}
---
+++ Classes - Exercise 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Classes___Exercise_2
+{
+    internal class Program
+    {
+        //Exercise 2: Design a StackOverflow Post
+        
+
+        //Design a class called Post.
+        //This class models a StackOverflow post.
+        //It should have properties for title, description and the date/time it was created.
+        //We should be able to up-vote or down-vote a post.
+        //We should also be able to see the current vote value.
+        //In the main method, create a post, up-vote and down-vote it a few times and then display the the current vote value.
+        //In this exercise, you will learn that a StackOverflow post should provide methods for up-voting and down-voting.
+        //You should not give the ability to set the Vote property from the outside, because otherwise, you may accidentally change the votes of a class to 0 or to a random number.
+        //And this is how we create bugs in our programs.
+        //The class should always protect its state and hide its implementation detail.
+        
+        //Educational tip: The aim of this exercise is to help you understand that classes should encapsulate data AND behaviour around that data.
+        //Many developers (even those with years of experience) tend to create classes that are purely data containers, and other classes that are purely behaviour (methods) providers.
+        //This is not object-oriented programming. This is procedural programming. Such programs are very fragile. Making a change breaks many parts of the code.
+
+        static void Main(string[] args)
+        {
+            Post myPostClass = new Post();
+            myPostClass.CommandList();
+
+            while (true)
+            {
+                Console.WriteLine("Please enter a command, when you are done please type END to publish the post and exit");
+                string commandInput = Console.ReadLine();
+                if (commandInput.ToUpper() == "COMMANDS")
+                {
+                    myPostClass.CommandList();
+                }
+                else if (commandInput.ToUpper() == "NAME")
+                {
+                    myPostClass.NameAndDescribePost();
+                }
+                else if (commandInput.ToUpper() == "UPVOTE")
+                {
+                    myPostClass.UpVotePost();
+                }
+                else if (commandInput.ToUpper() == "DOWNVOTE")
+                {
+                    myPostClass.DownVotePost();
+                }
+                else if (commandInput.ToUpper() == "COUNT")
+                {
+                    myPostClass.DisplayCurrentVoteValue();
+                }
+                else if (commandInput.ToUpper() == "PREVIEW")
+                {
+                    myPostClass.PreviewPost();
+                }
+                else if (commandInput.ToUpper() == "END")
+                {
+                    myPostClass.PreviewPost();
+                    break;
+                }
+            }
+        }
+    }
+}
